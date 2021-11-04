a2674035a219f839b65abbe094e5743a3284c320
Greg Smelkov <gsmelkov@gmail.com>
2021-11-03 21:43:31 -0400

    Start on game

---
+++ main.py
@@ -0,0 +1,155 @@
+from copy import deepcopy
+
+
+def emptyBoard():
+    board = []
+    for i in range(9):
+        board.append([' '] * 9)
+    return board
+
+
+def initBoard(board):
+    board[4][4] = 'K'
+    board[3][0] = 'b'
+    board[4][0] = 'b'
+    board[5][0] = 'b'
+    board[4][1] = 'b'
+    board[0][3] = 'b'
+    board[0][4] = 'b'
+    board[0][5] = 'b'
+    board[1][4] = 'b'
+    board[3][8] = 'b'
+    board[4][8] = 'b'
+    board[5][8] = 'b'
+    board[4][7] = 'b'
+    board[8][3] = 'b'
+    board[8][4] = 'b'
+    board[8][5] = 'b'
+    board[7][4] = 'b'
+    board[4][2] = 'w'
+    board[4][3] = 'w'
+    board[4][5] = 'w'
+    board[4][6] = 'w'
+    board[2][4] = 'w'
+    board[3][4] = 'w'
+    board[5][4] = 'w'
+    board[6][4] = 'w'
+    board[0][0] = 'e'
+    board[0][8] = 'e'
+    board[8][0] = 'e'
+    board[8][8] = 'e'
+
+
+def getMovesForPiece(board, x, y):
+    moves = []
+    p = board[x][y]
+    nx = x
+    while nx < 8:
+        nx += 1
+        if board[nx][y] == ' ' or p == 'K' and board[nx][y] == 'e':
+            moves.append((nx, y))
+        else:
+            break
+    nx = x
+    while nx > 0:
+        nx -= 1
+        if board[nx][y] == ' ' or p == 'K' and board[nx][y] == 'e':
+            moves.append((nx, y))
+        else:
+            break
+    ny = y
+    while ny < 8:
+        ny += 1
+        if board[x][ny] == ' ' or p == 'K' and board[x][ny] == 'e':
+            moves.append((x, ny))
+        else:
+            break
+    ny = y
+    while ny > 0:
+        ny -= 1
+        if board[x][ny] == ' ' or p == 'K' and board[x][ny] == 'e':
+            moves.append((x, ny))
+        else:
+            break
+
+    if (4, 4) in moves:
+        moves.remove((4, 4))
+
+    return moves
+
+
+def getPiecesToMove(board, p):
+    pieces = []
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == p or p == 'w' and board[i][j] == 'K':
+                pieces.append((i, j))
+    return pieces
+
+
+def getAllPossibleMoves(board, p):
+    moves = []
+    for piece in getPiecesToMove(board, p):
+        for move in getMovesForPiece(board, piece[0], piece[1]):
+            moves.append((piece, move))
+    return moves
+
+
+def getNextBoard(board, move):
+    nextBoard = deepcopy(board)
+    old = move[0]
+    new = move[1]
+    nextBoard[new[0]][new[1]] = nextBoard[old[0]][old[1]]
+    nextBoard[old[0]][old[1]] = ' '
+
+    return nextBoard
+
+
+def getAllNextBoards(board, p):
+    moves = getAllPossibleMoves(board, p)
+    boards = []
+    for move in moves:
+        boards.append(getNextBoard(board, move))
+    return boards
+
+
+def checkGameEnd(boards):
+    p = 'w' if len(boards) % 2 == 0 else 'b'
+    np = 'b' if p == 'w' else 'w'
+
+    if len(boards) > 4 and boards[-1] == boards[-5]:
+        return 'draw'
+    
+    if len(getAllPossibleMoves(boards[-1], np)) == 0:
+        return np
+    
+    if boards[-1][0][0] == 'K' or \
+        boards[-1][8][0] == 'K' or \
+        boards[-1][0][8] == 'K' or \
+        boards[-1][8][8] == 'K':
+        return 'w'
+
+    return 'in progress'
+
+
+def printBoard(board):
+    for i in range(9):
+        for j in range(9):
+            print(board[i][j], end=' ')
+        print()
+
+
+def main():
+    print("Hello World!")
+    board = emptyBoard()
+    initBoard(board)
+
+    printBoard(board)
+
+    for board in getAllNextBoards(board, 'w'):
+        printBoard(board)
+        print()
+
+
+if __name__ == '__main__':
+    main()
